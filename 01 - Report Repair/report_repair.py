2f4566b81b4f0dfdcfc20b3cfb8117d692e57d1b
Sofi <6748208-imsofi@users.noreply.gitlab.com>
2020-12-02 13:50:55 +0100

    Updated code styling to be more consistent

--- 01 - Report Repair/report_repair.py
+++ 01 - Report Repair/report_repair.py
@@ -9,10 +9,10 @@ def report_prod(report: list, sum_: int, permutations_: int):
         if sum(numbers) == sum_:
             return prod(numbers)
 
-print('Q1:', 'Find the two entries that sum to 2020; what do you get if you',
-      'multiply them together?')
-print('A1:', report_prod(report=expenses, sum_=2020, permutations_=2))
+print("Q1:", "Find the two entries that sum to 2020; what do you get if you",
+      "multiply them together?")
+print("A1:", report_prod(report=expenses, sum_=2020, permutations_=2))
 
-print('Q2:', 'In your expense report, what is the product of the three',
-      'entries that sum to 2020?')
-print('A2:', report_prod(report=expenses, sum_=2020, permutations_=3))
+print("Q2:", "In your expense report, what is the product of the three",
+      "entries that sum to 2020?")
+print("A2:", report_prod(report=expenses, sum_=2020, permutations_=3))
--- 02 - Password Philosophy/password_philosophy.py
+++ 02 - Password Philosophy/password_philosophy.py
@@ -1,7 +1,10 @@
 import re
 
+pattern = re.compile(
+    '(?P<min>^\d+)-(?P<max>\d+) (?P<letter>\w+): (?P<secret>.+)'
+)
+
 with open('input.txt', 'r') as f:
-    pattern = re.compile('(?P<min>^\d+)-(?P<max>\d+) (?P<letter>\w+): (?P<secret>.+)')
     passwords = []
     for line in f.read().splitlines():
         match = re.match(pattern, line)
@@ -19,7 +22,9 @@ def valid_password_by_pos(password: dict) -> bool:
 
     return (secret[min_] == letter) != (secret[max_] == letter)
 
-print('Q1:', 'How many passwords are valid according to their policies?')
-print('A1:', sum(valid_password_by_chars(password) for password in passwords))
-print('Q2:', 'How many passwords are valid according to the new interpretation of the policies?')
-print('A2:', sum(valid_password_by_pos(password) for password in passwords))
+print("Q1:", "How many passwords are valid according to their policies?")
+print("A1:", sum(valid_password_by_chars(password) for password in passwords))
+
+print("Q2:", "How many passwords are valid according to the new",
+      "interpretation of the policies?")
+print("A2:", sum(valid_password_by_pos(password) for password in passwords))
