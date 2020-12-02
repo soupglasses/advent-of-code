a4f98dbaa7b711cd7e97469764a1dd9ddf16bcd9
Sofi <6748208-imsofi@users.noreply.gitlab.com>
2020-12-02 15:23:13 +0100

    Use keyword arguments over passing dict into functions

--- 02 - Password Philosophy/password_philosophy.py
+++ 02 - Password Philosophy/password_philosophy.py
@@ -10,21 +10,17 @@ with open('input.txt', 'r') as f:
         match = re.match(pattern, line)
         passwords.append(match.groupdict())
 
-def valid_password_by_chars(password: dict) -> bool:
-    low, high = int(password['low']), int(password['high'])
-    letter, secret = password['letter'], password['secret']
-    
+def valid_password_by_letters(low: str, high: str, letter: str, secret: str) -> bool:
+    low, high = int(low), int(high)
     return low <= secret.count(letter) <= high
 
-def valid_password_by_pos(password: dict) -> bool:
-    low, high = int(password['low']) - 1, int(password['high']) - 1
-    letter, secret = password['letter'], password['secret']
-
+def valid_password_by_pos(low:str, high: str, letter: str, secret: str) -> bool:
+    low, high = int(low)-1, int(high)-1
     return (secret[low] == letter) != (secret[high] == letter)
 
 print("Q1:", "How many passwords are valid according to their policies?")
-print("A1:", sum(valid_password_by_chars(password) for password in passwords))
+print("A1:", sum(valid_password_by_letters(**password) for password in passwords))
 
 print("Q2:", "How many passwords are valid according to the new",
       "interpretation of the policies?")
-print("A2:", sum(valid_password_by_pos(password) for password in passwords))
+print("A2:", sum(valid_password_by_pos(**password) for password in passwords))
