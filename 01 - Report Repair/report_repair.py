147cb6b60c4e5b119424c8abaa31ffee9ef0e669
Sofi <6748208-imsofi@users.noreply.gitlab.com>
2020-12-02 17:14:10 +0100

    Better variable naming

--- 01 - Report Repair/report_repair.py
+++ 01 - Report Repair/report_repair.py
@@ -4,15 +4,15 @@ from math import prod
 with open('input.txt', 'r') as f:
     expenses = [int(line) for line in f.readlines()]
 
-def report_prod(report: list, sum_: int, permutations_: int):
-    for numbers in permutations(report, permutations_):
-        if sum(numbers) == sum_:
-            return prod(numbers)
+def prod_from_sum(numbers: list, sum_: int, width: int):
+    for expr_terms in permutations(numbers, width):
+        if sum(expr_terms) == sum_:
+            return prod(expr_terms)
 
 print("Q1:", "Find the two entries that sum to 2020; what do you get if you",
       "multiply them together?")
-print("A1:", report_prod(report=expenses, sum_=2020, permutations_=2))
+print("A1:", prod_from_sum(expenses, sum_=2020, width=2))
 
 print("Q2:", "In your expense report, what is the product of the three",
       "entries that sum to 2020?")
-print("A2:", report_prod(report=expenses, sum_=2020, permutations_=3))
+print("A2:", prod_from_sum(expenses, sum_=2020, width=3))
