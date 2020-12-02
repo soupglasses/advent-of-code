fa243a1950e0dd21796dfe0ab1c056325c8e3106
Sofi <6748208-imsofi@users.noreply.gitlab.com>
2020-12-02 13:41:48 +0100

    Day 2

---
+++ 02 - Password Philosophy/password_philosophy.py
@@ -0,0 +1,25 @@
+import re
+
+with open('input.txt', 'r') as f:
+    pattern = re.compile('(?P<min>^\d+)-(?P<max>\d+) (?P<letter>\w+): (?P<secret>.+)')
+    passwords = []
+    for line in f.read().splitlines():
+        match = re.match(pattern, line)
+        passwords.append(match.groupdict())
+
+def valid_password_by_chars(password: dict) -> bool:
+    min_, max_ = int(password['min']), int(password['max'])
+    letter, secret = password['letter'], password['secret']
+    
+    return min_ <= secret.count(letter) <= max_
+
+def valid_password_by_pos(password: dict) -> bool:
+    min_, max_ = int(password['min']) - 1, int(password['max']) - 1
+    letter, secret = password['letter'], password['secret']
+
+    return (secret[min_] == letter) != (secret[max_] == letter)
+
+print('Q1:', 'How many passwords are valid according to their policies?')
+print('A1:', sum(valid_password_by_chars(password) for password in passwords))
+print('Q2:', 'How many passwords are valid according to the new interpretation of the policies?')
+print('A2:', sum(valid_password_by_pos(password) for password in passwords))
