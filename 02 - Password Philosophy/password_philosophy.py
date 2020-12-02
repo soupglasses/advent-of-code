0ba3e2a5d714c2ebb6b66776840b2152cb43d6ef
Sofi <6748208-imsofi@users.noreply.gitlab.com>
2020-12-02 15:05:01 +0100

    Better variable naming for numbers

--- 02 - Password Philosophy/password_philosophy.py
+++ 02 - Password Philosophy/password_philosophy.py
@@ -1,7 +1,7 @@
 import re
 
 pattern = re.compile(
-    '(?P<min>^\d+)-(?P<max>\d+) (?P<letter>\w+): (?P<secret>.+)'
+    '(?P<low>^\d+)-(?P<high>\d+) (?P<letter>\w+): (?P<secret>.+)'
 )
 
 with open('input.txt', 'r') as f:
@@ -11,16 +11,16 @@ with open('input.txt', 'r') as f:
         passwords.append(match.groupdict())
 
 def valid_password_by_chars(password: dict) -> bool:
-    min_, max_ = int(password['min']), int(password['max'])
+    low, high = int(password['low']), int(password['high'])
     letter, secret = password['letter'], password['secret']
     
-    return min_ <= secret.count(letter) <= max_
+    return low <= secret.count(letter) <= high
 
 def valid_password_by_pos(password: dict) -> bool:
-    min_, max_ = int(password['min']) - 1, int(password['max']) - 1
+    low, high = int(password['low']) - 1, int(password['high']) - 1
     letter, secret = password['letter'], password['secret']
 
-    return (secret[min_] == letter) != (secret[max_] == letter)
+    return (secret[low] == letter) != (secret[high] == letter)
 
 print("Q1:", "How many passwords are valid according to their policies?")
 print("A1:", sum(valid_password_by_chars(password) for password in passwords))
