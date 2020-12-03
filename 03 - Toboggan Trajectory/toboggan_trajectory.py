adab61cb1c184b2a34f5e0a341909c190f13ed51
Sofi <6748208-imsofi@users.noreply.gitlab.com>
2020-12-03 16:39:56 +0100

    Abstracted into functions

--- 03 - Toboggan Trajectory/toboggan_trajectory.py
+++ 03 - Toboggan Trajectory/toboggan_trajectory.py
@@ -1,21 +1,21 @@
 from math import prod
 
+SLOPES = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+
 with open('input.txt', 'r') as f:
     grid_map = f.read().splitlines()
 
-a = 0
-for line_nr, coord_nr in zip(range(0, len(grid_map), 1), range(0, 100_000_000, 3)):
-    hit = grid_map[line_nr % len(grid_map)][coord_nr % len(grid_map[0])]
-    if hit == '#':
-        a += 1
-print('A1:', a)
-
-total = []
-for right, down in ((1,1), (3,1), (5,1), (7,1), (1,2)):
-    a = 0
-    for line_nr, coord_nr in zip(range(0, len(grid_map), down), range(0, 100_000_000, right)):
-        hit = grid_map[line_nr % len(grid_map)][coord_nr % len(grid_map[0])]
+def count_trees(grid: list, right: int, down: int) -> int:
+    trees = 0
+    width, height = len(grid[0]), len(grid)
+    for pos_down, pos_right in zip(range(0, height, down), range(0, height*right, right)):
+        hit = grid[pos_down % height][pos_right % width]
         if hit == '#':
-            a += 1
-    total.append(a)
-print('A2:', prod(total))
+            trees += 1
+    return trees
+
+def prod_of_slopes(grid: list, slopes: list):
+    return prod(count_trees(grid, right, down) for right, down in slopes)
+
+print('A1:', count_trees(grid_map, 3, 1))
+print('A2:', prod_of_slopes(grid_map, SLOPES))
