59e25b1b73c2aad975b3ae3491d91030847ea32c
Sofi <6748208-imsofi@users.noreply.gitlab.com>
2020-12-04 17:16:16 +0100

    Split logic into functions

--- 2020/04 - Passport Processing/passport_processing.py
+++ 2020/04 - Passport Processing/passport_processing.py
@@ -1,29 +1,15 @@
 from pprint import pprint
+from functools import partial
 import re
 
-naive_rules = ('byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid')
+FIELDS = {'byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'}
+VALID_EYE_COLORS = {'amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'}
 
-rules = {
-    'byr': {'low': 1920, 'high': 2002},
-    'iyr': {'low': 2010, 'high': 2020},
-    'eyr': {'low': 2020, 'high': 2030},
-    'hgt': {'cm': {'low': 150, 'high': 193},
-            'in': {'low': 59, 'high': 76}},
-    'hcl': {'re': '^#[0-9a-f]{6}$'},
-    'ecl': {'cl': ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']},
-    'pid': {'re': '^[0-9]{9}$'},
-    'cid': {},
-}
+RE_HAIR_COLOR = re.compile('^#[0-9a-f]{6}$')
+RE_PID = re.compile('^[0-9]{9}$')
 
 with open('input.txt', 'r') as f:
     raw_passports = f.read()
-
-def is_int(value):
-    try: 
-        int(value)
-        return True
-    except ValueError:
-        return False
 
 def parse_passport(raw_passport: str) -> dict:
     passport = {}
@@ -38,58 +24,52 @@ def parse_passports(raw_passports: str) -> list[dict]:
         passports.append(parse_passport(raw_passport))
     return passports
 
-def naive_passport_validation(passport: dict, rules: list) -> bool:
-    return all(rule in passport for rule in rules)
+def validate_year_range(year: str, low: str, high: str) -> bool:
+    return low <= year <= high
 
-def check_rule(rule_key: str, value: str, rules: dict = rules):
-    if not rule_key in rules:
-        print(f"{rule_key} not in rules.")
-        return False
-    rule = rules[rule_key]
-    t = False
-    if rule_key in ('byr', 'iyr', 'eyr'):
-        if is_int(value) and rule['low'] <= int(value) <= rule['high']:
-            t = True
-        else:
-            print(f"{value} not in range {rule['low']} {rule['high']}")
-    elif rule_key == 'hgt':
-        m, h = value[-2:], value[:-2]
-        if m in ('cm', 'in'):
-            if is_int(h) and rule[m]['low'] <= int(h) <= rule[m]['high']:
-                t = True
-            else:
-                print(f"{value} not in range {rule[m]['low']} {rule[m]['high']}")
-        else:
-            print(f"{value} not valid height measurement.")
-    elif rule_key == 'hcl':
-        if bool(re.match(rule['re'], value)):
-            t = True
-        else:
-            print(f"{value} is not a valid hair color.")
-    elif rule_key == 'ecl':
-        if value in rule['cl']:
-            t = True
-        else:
-            print(f"{value} not a valid eye color.")
-    elif rule_key == 'pid':
-        if bool(re.match(rule['re'], value)):
-            t = True
-        else:
-            print(f"{value} not valid passport id.")
-    elif rule_key == 'cid':
-        t = True
-    return t
+def validate_height(height: str) -> bool:
+    measure, height = height[-2:], height[:-2]
+    if measure == 'cm':
+        return '150' <= height <= '193'
+    if measure == 'in':
+        return '59' <= height <= '76'
 
-def passport_validation(passport: dict, rules: dict) -> bool:
-    x = naive_passport_validation(passport, naive_rules)
-    y = all(check_rule(key, value, rules) for key, value in passport.items())
-    print(x, y)
-    return x and y
+def validate_hair_color(color: str) -> bool:
+    return bool(RE_HAIR_COLOR.match(color))
+
+def validate_eye_color(color: str) -> bool:
+    return color in VALID_EYE_COLORS
+
+def validate_pid(pid: str) -> bool:
+    return bool(RE_PID.match(pid))
+
+def validate_cid(cid) -> bool:
+    return True
+
+validate_key = {
+    'byr': partial(validate_year_range, low='1920', high='2002'),
+    'iyr': partial(validate_year_range, low='2010', high='2020'),
+    'eyr': partial(validate_year_range, low='2020', high='2030'),
+    'hgt': validate_height,
+    'hcl': validate_hair_color,
+    'ecl': validate_eye_color,
+    'pid': validate_pid,
+    'cid': validate_cid,
+}
+
+def check_passport_fields(passport: dict, fields: set) -> bool:
+    return all(rule in passport for rule in fields)
+
+def passport_validation(passport: dict, fields: set) -> bool:
+    return (
+        check_passport_fields(passport, fields)
+        and all(validate_key[key](value) for key, value in passport.items())
+    )
 
 if __name__ == '__main__':
     passports = parse_passports(raw_passports)
-    print('A1:', sum(naive_passport_validation(passport, naive_rules)
+    print('A1:', sum(check_passport_fields(passport, FIELDS)
                      for passport in passports))
 
-    print('A2:', sum(passport_validation(passport, rules)
+    print('A2:', sum(passport_validation(passport, FIELDS)
                      for passport in passports))
