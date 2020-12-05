461be5c28263bfae121c461c265022acb4062efc
Sofi <6748208-imsofi@users.noreply.gitlab.com>
2020-12-05 07:53:51 +0100

    Simplify functions

--- 2020/05 - Binary Boarding/binary_boarding.py
+++ 2020/05 - Binary Boarding/binary_boarding.py
@@ -1,29 +1,20 @@
 
+bp_binary = str.maketrans('FLBR', '0011')
+
 with open('input.txt', 'r') as f:
     boarding_passes = f.read().splitlines()
 
-def parse_seat_pos(board_pass: str) -> int:
-    limit = (0, 2 ** len(board_pass) - 1)
-    for step in board_pass:
-        split = sum(limit) // 2
-        lower, upper = limit
-        if step in 'FL':
-            limit = (lower, split)
-        else:
-            limit = (split+1, upper)
-    return limit[0]
 
-def seat_pos(board_pass: str) -> tuple[int, int]:
-    bp_row, bp_col = board_pass[:7], board_pass[7:]
-    row, col = parse_seat_pos(bp_row), parse_seat_pos(bp_col)
-    return row, col
+def seat_pos(board_pass: str) -> int:
+    return int(board_pass.translate(bp_binary), 2)
 
 def seat_id(board_pass: str) -> int:
-    row, col = seat_pos(board_pass)
+    row, col = seat_pos(board_pass[:7]), seat_pos(board_pass[7:])
     return row * 8 + col
 
 def find_missing(lst: list) -> int:
-    return next((x for x in range(lst[0], lst[-1]+1) if x not in lst))
+    return next(i for i in range(lst[0], lst[-1]+1) if i not in lst)
+
 
 if __name__ == '__main__':
     taken_seat_ids = [seat_id(board_pass) for board_pass in boarding_passes]
