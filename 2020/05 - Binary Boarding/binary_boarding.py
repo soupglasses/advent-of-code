b69d4df6e4f5a7bb66aaf605795790900cb315c3
Sofi <6748208-imsofi@users.noreply.gitlab.com>
2020-12-05 07:36:32 +0100

    Added extra typehints

--- 2020/05 - Binary Boarding/binary_boarding.py
+++ 2020/05 - Binary Boarding/binary_boarding.py
@@ -2,7 +2,7 @@
 with open('input.txt', 'r') as f:
     boarding_passes = f.read().splitlines()
 
-def parse_seat_pos(board_pass: str):
+def parse_seat_pos(board_pass: str) -> int:
     limit = (0, 2 ** len(board_pass) - 1)
     for step in board_pass:
         split = sum(limit) // 2
@@ -13,16 +13,16 @@ def parse_seat_pos(board_pass: str):
             limit = (split+1, upper)
     return limit[0]
 
-def seat_pos(board_pass: str):
+def seat_pos(board_pass: str) -> tuple[int]:
     bp_row, bp_col = board_pass[:7], board_pass[7:]
     row, col = parse_seat_pos(bp_row), parse_seat_pos(bp_col)
     return row, col
 
-def seat_id(board_pass: str):
+def seat_id(board_pass: str) -> int:
     row, col = seat_pos(board_pass)
     return row * 8 + col
 
-def find_missing(lst: list):
+def find_missing(lst: list) -> int:
     return next((x for x in range(lst[0], lst[-1]+1) if x not in lst))
 
 if __name__ == '__main__':
