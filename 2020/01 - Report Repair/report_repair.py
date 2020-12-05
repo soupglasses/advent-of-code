f45fb34cc3897407afec2b231020e8ae4053dccf
Sofi <6748208-imsofi@users.noreply.gitlab.com>
2020-12-05 22:22:46 +0100

    Fix strings

--- 2020/01 - Report Repair/report_repair.py
+++ 2020/01 - Report Repair/report_repair.py
@@ -12,8 +12,8 @@ def prod_from_sum(numbers: list, sum_: int, expr_len: int):
 if __name__ == '__main__':
     print('Q1:', 'Find the two entries that sum to 2020; what do you get',
           'if you multiply them together?')
-    print("A1:", prod_from_sum(expenses, sum_=2020, expr_len=2))
+    print('A1:', prod_from_sum(expenses, sum_=2020, expr_len=2))
 
-    print('Q2:', "In your expense report, what is the product of the three',
+    print('Q2:', 'In your expense report, what is the product of the three',
           'entries that sum to 2020?')
     print('A2:', prod_from_sum(expenses, sum_=2020, expr_len=3))
