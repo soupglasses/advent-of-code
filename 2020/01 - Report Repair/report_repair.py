806c89a25a1da0788590b97226d15684a06570de
Sofi <6748208-imsofi@users.noreply.gitlab.com>
2020-12-05 22:21:08 +0100

    Put run code in a if block

--- 2020/01 - Report Repair/report_repair.py
+++ 2020/01 - Report Repair/report_repair.py
@@ -9,10 +9,11 @@ def prod_from_sum(numbers: list, sum_: int, expr_len: int):
         if sum(expr_terms) == sum_:
             return prod(expr_terms)
 
-print("Q1:", "Find the two entries that sum to 2020; what do you get if you",
-      "multiply them together?")
-print("A1:", prod_from_sum(expenses, sum_=2020, expr_len=2))
+if __name__ == '__main__':
+    print('Q1:', 'Find the two entries that sum to 2020; what do you get',
+          'if you multiply them together?')
+    print("A1:", prod_from_sum(expenses, sum_=2020, expr_len=2))
 
-print("Q2:", "In your expense report, what is the product of the three",
-      "entries that sum to 2020?")
-print("A2:", prod_from_sum(expenses, sum_=2020, expr_len=3))
+    print('Q2:', "In your expense report, what is the product of the three',
+          'entries that sum to 2020?')
+    print('A2:', prod_from_sum(expenses, sum_=2020, expr_len=3))
