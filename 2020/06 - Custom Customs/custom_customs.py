980622b635ddf1bade4cd4abb2593fd8a4986a43
Sofi <6748208-imsofi@users.noreply.gitlab.com>
2020-12-06 13:20:57 +0100

    Use descriptive function names

--- 2020/06 - Custom Customs/custom_customs.py
+++ 2020/06 - Custom Customs/custom_customs.py
@@ -1,10 +1,10 @@
-def part_1(forms: list) -> int:
+def count_any(forms: list) -> int:
     return sum(
         len(set(''.join(group.split())))
         for group in forms
     )
 
-def part_2(forms: list) -> int:
+def count_all(forms: list) -> int:
     return sum(
         len(set.intersection(*map(set, group.splitlines())))
         for group in forms
@@ -17,8 +17,8 @@ if __name__ == '__main__':
 
     print('Q1:', 'For each group, count the number of questions to which',
           'anyone answered "yes". What is the sum of those counts?')
-    print('A1:', part_1(forms))
+    print('A1:', count_any(forms))
 
     print('Q2:', 'For each group, count the number of questions to which',
           'everyone answered "yes". What is the sum of those counts?')
-    print('A2:', part_2(forms))
+    print('A2:', count_all(forms))
