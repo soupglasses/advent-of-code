45e1b8336bb4a514273ad754092a7790dd9108d7
Sofi <6748208-imsofi@users.noreply.gitlab.com>
2020-12-11 05:17:08 +0100

    Better naming of functions and variables

--- 2020/08 - Handheld Halting/handheld_halting.py
+++ 2020/08 - Handheld Halting/handheld_halting.py
@@ -1,4 +1,4 @@
-def break_at_recursion(stepper):
+def stop_at_recursion(stepper):
     def stepper_wrapper(prog: list, step: int = 0, acc: int = 0):
         steps = set()
         run = stepper(prog, step, acc)
@@ -13,8 +13,8 @@ def break_at_recursion(stepper):
         return run
     return stepper_wrapper
 
-@break_at_recursion
-def stepper(prog: list, step: int = 0, acc: int = 0):
+@stop_at_recursion
+def stepper(prog: list, step: int, acc: int):
     op, i = prog[step]
     if op == 'nop':
         return {'prog': prog, 'step': step + 1, 'acc': acc}
@@ -23,26 +23,29 @@ def stepper(prog: list, step: int = 0, acc: int = 0):
     elif op == 'jmp':
         return {'prog': prog, 'step': step + i, 'acc': acc}
 
-def part_2_deltas(prog: list) -> list:
+def generate_deltas(prog: list) -> list:
     return [(step, 'jmp', i) if op == 'nop'
             else (step, 'nop', i)
             for step, (op, i) in enumerate(prog)
             if op in ('jmp', 'nop')]
 
-def terminating_delta(prog: list, prog_deltas: list):
+def part_2(prog: list, prog_deltas: list):
+    prog = list(prog)
     for step, op, i in prog_deltas:
-        prog_with_delta = list(prog)
-        prog_with_delta[step] = (op, i)
-        ret_vals = stepper(prog_with_delta)
+        old_delta = prog[step]
+        prog[step] = (op, i)
+        ret_vals = stepper(prog)
+        prog[step] = old_delta
         if ret_vals['err'] is False:
             return ret_vals
 
 
 if __name__ == '__main__':
     with open('input.txt', 'r') as f:
-        prog = [(line.split()[0], int(line.split()[1]))
-                for line in f.read().splitlines()]
+        program = [(line.split()[0], int(line.split()[1]))
+                   for line in f.read().splitlines()]
 
-    print('A1:', stepper(prog)['acc'])
-    prog_deltas = part_2_deltas(prog)
-    print('A2:', terminating_delta(prog, prog_deltas)['acc'])
+    print('A1:', stepper(program)['acc'])
+
+    all_deltas = generate_deltas(program)
+    print('A2:', part_2(program, all_deltas)['acc'])
