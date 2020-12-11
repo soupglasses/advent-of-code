4833a8e4a114cc3dc74b96023dbdead4dc229536
Sofi <6748208-imsofi@users.noreply.gitlab.com>
2020-12-11 04:31:13 +0100

    Use more variable names and follow code styling of other days

--- 2020/10 - Adapter Array/adapter_array.py
+++ 2020/10 - Adapter Array/adapter_array.py
@@ -1,9 +1,5 @@
 from functools import lru_cache
 
-with open('input.txt', 'r') as f:
-    adapters = [int(i) for i in f.read().splitlines()]
-
-adapters = [0] + sorted(adapters) + [max(adapters) + 3]
 
 def part_1():
     diffs = [0, 0, 0]
@@ -12,13 +8,15 @@ def part_1():
         diffs[diff - 1] += 1
     return diffs[0] * diffs[2]
 
-@lru_cache()
+@lru_cache
 def part_2(s_pos: int = 0):
     count = 0
     end = adapters[-1]
+    s_val = adapters[s_pos]
     for n_pos in range(s_pos + 1, len(adapters)):
-        if adapters[n_pos] - adapters[s_pos] <= 3:
-            if end == adapters[n_pos]:
+        n_val = adapters[n_pos]
+        if n_val - s_val <= 3:
+            if n_val == end:
                 count += 1
             count += part_2(n_pos)
         else:
@@ -26,5 +24,11 @@ def part_2(s_pos: int = 0):
     return count
 
 
-print('A1:', part_1())
-print('A2:', part_2())
+if __name__ == '__main__':
+    with open('input.txt', 'r') as f:
+        adapters = [int(i) for i in f.read().splitlines()]
+
+    adapters = [0] + sorted(adapters) + [max(adapters) + 3]
+
+    print('A1:', part_1())
+    print('A2:', part_2())
