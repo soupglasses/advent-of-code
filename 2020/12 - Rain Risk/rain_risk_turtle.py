2fb5ec08e9c4c145288208a96c914bb0fdd53093
Sofi <6748208-imsofi@users.noreply.gitlab.com>
2020-12-12 20:40:54 +0100

    Add part 1 turtle solution

---
+++ 2020/12 - Rain Risk/rain_risk_turtle.py
@@ -0,0 +1,40 @@
+import turtle
+
+DIRECTIONS = {'N': 90, 'S': 270, 'E': 0, 'W': 180}
+
+def move_direction(direction: str, distance: int):
+    old_heading = turtle.heading()
+    direction = DIRECTIONS[direction]
+    turtle.setheading(direction)
+    turtle.forward(distance)
+    turtle.setheading(old_heading)
+
+def do_action(action: str, value: int):
+    if action == 'F':
+        turtle.forward(value)
+    elif action == 'L':
+        turtle.left(value)
+    elif action == 'R':
+        turtle.right(value)
+    elif action in 'NSEW':
+        move_direction(action, value)
+    else:
+        raise NotImplementedError
+
+
+if __name__ == '__main__':
+    turtle.speed(0)      # max speed
+    turtle.tracer(0, 0)  # disable screen refreshing
+
+    with open('input.txt', 'r') as f:
+        instructions = [(line[0], int(line[1:]))
+                        for line in f.read().splitlines()]
+
+    for action, value in instructions:
+        do_action(action, value)
+
+    east, north = map(abs, turtle.pos())
+    print('A1:', int(east + north))
+
+    turtle.update()  # refresh screen
+    turtle.done()    # show window till user input
