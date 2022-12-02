4153b3a01ef496e57d274a4a1bdc115753fd63d3
Sofi <sofi+git@mailbox.org>
2022-12-02 09:41:44 +0100

    ruby: optimize day 1

--- 2022/day_01.rb
+++ 2022/day_01.rb
@@ -5,13 +5,14 @@ file = ARGV[0] || File.join(__dir__, "inputs", "example_01.txt")
 sums_of_calories = File.read(file)
   .split("\n\n")
   .map { _1.split.map(&:to_i).sum }
+  .sort
 
 def part_1(data)
-  data.max
+  data.last
 end
 
 def part_2(data)
-  data.sort.last(3).sum
+  data.last(3).sum
 end
 
 puts part_1(sums_of_calories)
