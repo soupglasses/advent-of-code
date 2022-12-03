e1ef6d1e6aea30ee93e8c7bf6e23a67d3ee4b8bc
Sofi <sofi+git@mailbox.org>
2022-12-03 17:27:49 +0100

    ruby: rewrite helper function

--- 2022/day_03.rb
+++ 2022/day_03.rb
@@ -7,29 +7,23 @@ path = ARGV[0] || File.join(__dir__, "inputs", "example_03.txt")
 rucksacks = File.read(path)
   .split("\n")
 
-def letter_priority(char)
-  char.ord - (char == char.downcase ? 96 : 38)
-end
-
-def reduce_to_letter_priorities(sets)
-  sets
-    .map { _1.reduce(&:intersection) }
-    .map(&:first)
-    .map { letter_priority _1 }
-    .sum
+def reduce_to_item_priority(sets)
+  (sets.reduce(&:intersection).first.ord + 20) % 58
 end
 
 def part_1(data)
   data
     .map { _1.each_char.each_slice(_1.size / 2).map(&:to_set) }
-    .then { reduce_to_letter_priorities _1 }
+    .map { reduce_to_item_priority _1 }
+    .sum
 end
 
 def part_2(data)
   data
     .map { _1.each_char.to_set }
     .each_slice(3)
-    .then { reduce_to_letter_priorities _1 }
+    .map { reduce_to_item_priority _1 }
+    .sum
 end
 
 puts part_1(rucksacks)
