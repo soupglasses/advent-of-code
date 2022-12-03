e93fdcd68287de4ce087b88bdf7880a4fff460fa
Sofi <sofi+git@mailbox.org>
2022-12-03 13:50:59 +0100

    ruby: simplify

--- 2022/day_03.rb
+++ 2022/day_03.rb
@@ -8,25 +8,28 @@ rucksacks = File.read(path)
   .split("\n")
 
 def letter_priority(char)
-  char.ord - (char.match?(/[a-z]/) ? "a".ord - 1 : "A".ord - 27)
+  char.ord - (char == char.downcase ? 96 : 38)
+end
+
+def reduce_sets_to_letter_priorities(sets)
+  sets
+    .map { _1.reduce(&:intersection) }
+    .map(&:first)
+    .map { letter_priority _1 }
+    .sum
 end
 
 def part_1(data)
   data
-    .map { _1.each_char.each_slice(_1.size / 2).map(&:join) }
-    .map { (_1[0].chars & _1[1].chars).first }
-    .map { letter_priority(_1) }
-    .sum
+    .map { _1.each_char.each_slice(_1.size / 2).map(&:to_set) }
+    .then { reduce_sets_to_letter_priorities _1 }
 end
 
 def part_2(data)
   data
     .map { _1.each_char.to_set }
     .each_slice(3)
-    .map { _1.reduce(&:intersection) }
-    .map(&:first)
-    .map { letter_priority(_1) }
-    .sum
+    .then { reduce_sets_to_letter_priorities _1 }
 end
 
 puts part_1(rucksacks)
