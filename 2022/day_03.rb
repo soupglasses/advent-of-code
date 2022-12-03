f6f5437d2bfc60900a948ae3c2306ea3272d1ceb
Sofi <sofi+git@mailbox.org>
2022-12-03 14:01:04 +0100

    ruby: simplify

--- 2022/day_03.rb
+++ 2022/day_03.rb
@@ -11,7 +11,7 @@ def letter_priority(char)
   char.ord - (char == char.downcase ? 96 : 38)
 end
 
-def reduce_sets_to_letter_priorities(sets)
+def reduce_to_letter_priorities(sets)
   sets
     .map { _1.reduce(&:intersection) }
     .map(&:first)
@@ -22,14 +22,14 @@ end
 def part_1(data)
   data
     .map { _1.each_char.each_slice(_1.size / 2).map(&:to_set) }
-    .then { reduce_sets_to_letter_priorities _1 }
+    .then { reduce_to_letter_priorities _1 }
 end
 
 def part_2(data)
   data
     .map { _1.each_char.to_set }
     .each_slice(3)
-    .then { reduce_sets_to_letter_priorities _1 }
+    .then { reduce_to_letter_priorities _1 }
 end
 
 puts part_1(rucksacks)
