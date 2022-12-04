05601dc85d23d342baf8dd24aef57de2d342978a
Sofi <sofi+git@mailbox.org>
2022-12-04 16:13:36 +0100

    ruby: go back to sets to avoid edge cases

--- 2022/day_04.rb
+++ 2022/day_04.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+require 'set'
+
 path = ARGV[0] || File.join(__dir__, "inputs", "example_04.txt")
 
 camp_sections = File.foreach(path, chomp: true)
@@ -8,22 +10,18 @@ camp_sections = File.foreach(path, chomp: true)
     .split(/,|-/)
     .map(&:to_i)
     .each_slice(2)
-    .map { _1.._2 }
+    .map { Range.new(_1, _2).to_set }
 }
-
-def disjoint?(r1, r2)
-  (r1.begin <= r2.begin && r2.begin <= r1.end) || (r2.begin <= r1.begin && r1.begin <= r2.end)
-end
 
 def part_1(data)
   data
-    .filter { _1.cover?(_2) || _2.cover?(_1) }
+    .filter { _1.subset?(_2) || _2.subset?(_1) }
     .count
 end
 
 def part_2(data)
   data
-    .filter { disjoint?(_1, _2) }
+    .filter { _1.intersect?(_2) }
     .count
 end
 
