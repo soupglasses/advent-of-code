4be88cac629737370599531c7c449ea55af55eb3
Sofi <sofi+git@mailbox.org>
2022-12-04 12:38:04 +0100

    complete day 4

---
+++ 2022/day_04.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+
+require "set"
+
+path = ARGV[0] || File.join(__dir__, "inputs", "example_04.txt")
+
+camp_sections = File.read(path)
+  .split("\n")
+  .map { |line|
+  line
+    .split(",")
+    .map { |section|
+    section
+      .split("-")
+      .map(&:to_i)
+      .then { Range.new(_1, _2) }
+      .to_set
+  }
+}
+
+def part_1(data)
+  data
+    .map { _1.subset?(_2) || _1.superset?(_2) }
+    .count(true)
+end
+
+def part_2(data)
+  data
+    .map { _1.intersect? _2 }
+    .count(true)
+end
+
+puts part_1(camp_sections)
+puts part_2(camp_sections)
