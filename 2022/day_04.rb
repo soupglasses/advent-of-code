5c54b4f6804b2a03715499dd2c691161ff368ccc
Sofi <sofi+git@mailbox.org>
2022-12-04 15:59:45 +0100

    ruby: work only with ranges

--- 2022/day_04.rb
+++ 2022/day_04.rb
@@ -1,28 +1,29 @@
 #!/usr/bin/env ruby
 
-require "set"
-
 path = ARGV[0] || File.join(__dir__, "inputs", "example_04.txt")
 
-camp_sections = File.read(path)
-  .split("\n")
+camp_sections = File.foreach(path, chomp: true)
   .map { |line|
   line
     .split(/,|-/)
     .map(&:to_i)
     .each_slice(2)
-    .map { Range.new(_1, _2).to_set }
+    .map { Range.new(_1, _2) }
 }
+
+def disjoint?(r1, r2)
+  (r1.begin <= r2.begin && r2.begin <= r1.end) || (r2.begin <= r1.begin && r1.begin <= r2.end)
+end
 
 def part_1(data)
   data
-    .filter { _1.subset?(_2) || _1.superset?(_2) }
+    .filter { _1.cover?(_2) || _2.cover?(_1) }
     .count
 end
 
 def part_2(data)
   data
-    .filter { _1.intersect?(_2) }
+    .filter { disjoint?(_1, _2) }
     .count
 end
 
