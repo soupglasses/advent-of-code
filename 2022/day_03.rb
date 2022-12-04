9b7aa7354f6485821f381a5b8321fee41b5eaa1a
Sofi <sofi+git@mailbox.org>
2022-12-04 12:38:22 +0100

    ruby: use set function

--- 2022/day_03.rb
+++ 2022/day_03.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+require 'set'
+
 path = ARGV[0] || File.join(__dir__, "inputs", "example_03.txt")
 
 rucksacks = File.read(path).split("\n")
@@ -10,14 +12,14 @@ end
 
 def part_1(data)
   data
-    .map { _1.each_char.each_slice(_1.size / 2) }
+    .map { _1.each_char.each_slice(_1.size / 2).map(&:to_set) }
     .map { reduce_to_item_priority _1 }
     .sum
 end
 
 def part_2(data)
   data
-    .map(&:chars)
+    .map { _1.each_char.to_set }
     .each_slice(3)
     .map { reduce_to_item_priority _1 }
     .sum
