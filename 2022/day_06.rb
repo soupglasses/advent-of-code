89b160da69622f5347cf69c533fd4c82af018999
Sofi <sofi+git@mailbox.org>
2022-12-06 11:40:46 +0100

    simplify function

--- 2022/day_06.rb
+++ 2022/day_06.rb
@@ -4,22 +4,20 @@ PATH = ARGV[0] || File.join(__dir__, "inputs", "example_06.txt")
 
 datastream = File.read(PATH).chomp
 
-def start_of_packet_by_size(data, size)
+def packet_offset_from_size(data, size)
   data
-    .chars
+    .each_char
     .each_cons(size)
-    .with_index
-    .filter { |a, _| a.uniq.length == a.length }
-    .first
-    .then { |_, i| i + size }
+    .find_index { _1.uniq.length == size }
+    .then { _1 + size }
 end
 
 def part_1(data)
-  start_of_packet_by_size(data, 4)
+  packet_offset_from_size(data, 4)
 end
 
 def part_2(data)
-  start_of_packet_by_size(data, 14)
+  packet_offset_from_size(data, 14)
 end
 
 puts part_1(datastream)
