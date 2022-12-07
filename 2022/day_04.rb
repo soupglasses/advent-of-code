2ac9f82482a2fa7c3d584b415e9a45fcb7a2f3d4
Sofi <sofi+git@mailbox.org>
2022-12-07 22:49:20 +0100

    extend Range for intersect? support

--- 2022/day_04.rb
+++ 2022/day_04.rb
@@ -1,29 +1,42 @@
 #!/usr/bin/env ruby
 
-require 'set'
-
 PATH = ARGV[0] || File.join(__dir__, "inputs", "example_04.txt")
 
-camp_sections = File.foreach(PATH, chomp: true)
-  .map { |line|
-  line
+class Range
+  def disjoint?(other)
+    not self.intersect?(other)
+  end
+
+  def intersect?(other)
+    # Uses `self.max` to support exclusive ranges correctly.
+    return true if self.begin <= other.begin && other.begin <= self.max
+    return true if other.begin <= self.begin && self.begin <= other.max
+  end
+
+  def intersection(other)
+    return nil if self.disjoint?(other)
+    Range.new([self.begin, other.begin].max, [self.max, other.max].min)
+  end
+
+  alias_method :&, :intersection
+end
+
+camp_assignment_pairs = File.read(PATH).chomp.split("\n")
+  .map { |section_pair|
+  section_pair
     .split(/,|-/)
     .map(&:to_i)
     .each_slice(2)
-    .map { Range.new(_1, _2).to_set }
+    .map { Range.new(_1, _2) }
 }
 
 def part_1(data)
-  data
-    .filter { _1.subset?(_2) || _2.subset?(_1) }
-    .count
+  data.count { _1.cover?(_2) || _2.cover?(_1) }
 end
 
 def part_2(data)
-  data
-    .filter { _1.intersect?(_2) }
-    .count
+  data.count { _1.intersect?(_2) }
 end
 
-puts part_1(camp_sections)
-puts part_2(camp_sections)
+puts part_1(camp_assignment_pairs)
+puts part_2(camp_assignment_pairs)
