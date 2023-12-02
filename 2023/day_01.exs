6a2c02039d13849ffd84f2ae663b07ee1dca8b99
Sofi <sofi+git@mailbox.org>
2023-12-02 03:09:15 +0100

    style: using domain spessific naming

--- 2023/day_01.exs
+++ 2023/day_01.exs
@@ -17,33 +17,33 @@ defmodule Day01 do
   def parse_digit_only(<<c>> <> rest) when c in ?0..?9, do: [c - ?0 | parse_digit_only(rest)]
   def parse_digit_only(<<_>> <> rest), do: parse_digit_only(rest)
 
-  def parse(""), do: []
-  def parse(<<c>>   <> rest) when c in ?0..?9, do: [c - ?0 | parse(rest)]
-  def parse("zero"  <> rest), do: [0 | parse("o" <> rest)]
-  def parse("one"   <> rest), do: [1 | parse("e" <> rest)]
-  def parse("two"   <> rest), do: [2 | parse("o" <> rest)]
-  def parse("three" <> rest), do: [3 | parse("e" <> rest)]
-  def parse("four"  <> rest), do: [4 | parse("r" <> rest)]
-  def parse("five"  <> rest), do: [5 | parse("e" <> rest)]
-  def parse("six"   <> rest), do: [6 | parse("x" <> rest)]
-  def parse("seven" <> rest), do: [7 | parse("n" <> rest)]
-  def parse("eight" <> rest), do: [8 | parse("t" <> rest)]
-  def parse("nine"  <> rest), do: [9 | parse("e" <> rest)]
-  def parse(<<_>>   <> rest), do: parse(rest)
+  def parse_both(""), do: []
+  def parse_both(<<c>>   <> rest) when c in ?0..?9, do: [c - ?0 | parse_both(rest)]
+  def parse_both("zero"  <> rest), do: [0 | parse_both("o" <> rest)]
+  def parse_both("one"   <> rest), do: [1 | parse_both("e" <> rest)]
+  def parse_both("two"   <> rest), do: [2 | parse_both("o" <> rest)]
+  def parse_both("three" <> rest), do: [3 | parse_both("e" <> rest)]
+  def parse_both("four"  <> rest), do: [4 | parse_both("r" <> rest)]
+  def parse_both("five"  <> rest), do: [5 | parse_both("e" <> rest)]
+  def parse_both("six"   <> rest), do: [6 | parse_both("x" <> rest)]
+  def parse_both("seven" <> rest), do: [7 | parse_both("n" <> rest)]
+  def parse_both("eight" <> rest), do: [8 | parse_both("t" <> rest)]
+  def parse_both("nine"  <> rest), do: [9 | parse_both("e" <> rest)]
+  def parse_both(<<_>>   <> rest), do: parse_both(rest)
 
-  def solve(str, func) do
-    String.split(str)
-    |> Enum.map(func)
+  def recalibrate_by(strategy, document) do
+    String.split(document)
+    |> Enum.map(strategy)
     |> Enum.map(fn digits -> List.first(digits) * 10 + List.last(digits) end)
     |> Enum.sum()
   end
 
-  def part1(str), do: solve(str, &parse_digit_only/1)
-  def part2(str), do: solve(str, &parse/1)
+  def part1(document), do: recalibrate_by(&parse_digit_only/1, document)
+  def part2(document), do: recalibrate_by(&parse_both/1, document)
 end
 
-part_1_data = File.read!("./inputs/example_01_1.txt")
-part_2_data = File.read!("./inputs/example_01_2.txt")
+part_1_document = File.read!("./inputs/example_01_1.txt")
+part_2_document = File.read!("./inputs/example_01_2.txt")
 
-IO.inspect(Day01.part1(part_1_data))
-IO.inspect(Day01.part2(part_2_data))
+IO.inspect(Day01.part1(part_1_document))
+IO.inspect(Day01.part2(part_2_document))
