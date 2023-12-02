37c2866f9b2cff67680b7d1c54a29f6282421826
Sofi <sofi+git@mailbox.org>
2023-12-02 02:57:53 +0100

    fix: accidentally broken part1

--- 2023/day_01.exs
+++ 2023/day_01.exs
@@ -13,6 +13,10 @@ defmodule Day01 do
     possible valid digits.
   """
 
+  def parse_digit_only(""), do: []
+  def parse_digit_only(<<c>> <> rest) when c in ?0..?9, do: [c - ?0 | parse_digit_only(rest)]
+  def parse_digit_only(<<_>> <> rest), do: parse_digit_only(rest)
+
   def parse(""), do: []
   def parse(<<c>>   <> rest) when c in ?0..?9, do: [c - ?0 | parse(rest)]
   def parse("zero"  <> rest), do: [0 | parse("o" <> rest)]
@@ -27,16 +31,19 @@ defmodule Day01 do
   def parse("nine"  <> rest), do: [9 | parse("e" <> rest)]
   def parse(<<_>>   <> rest), do: parse(rest)
 
-  def solve(str) do
+  def solve(str, func) do
     String.split(str)
-    |> Enum.map(&Day01.parse/1)
+    |> Enum.map(func)
     |> Enum.map(fn digits -> List.first(digits) * 10 + List.last(digits) end)
     |> Enum.sum()
   end
+
+  def part1(str), do: solve(str, &parse_digit_only/1)
+  def part2(str), do: solve(str, &parse/1)
 end
 
-part_1 = File.read!("./inputs/example_01_1.txt")
-part_2 = File.read!("./inputs/example_01_2.txt")
+part_1_data = File.read!("./inputs/example_01_1.txt")
+part_2_data = File.read!("./inputs/example_01_2.txt")
 
-IO.inspect(Day01.solve(part_1))
-IO.inspect(Day01.solve(part_2))
+IO.inspect(Day01.part1(part_1_data))
+IO.inspect(Day01.part2(part_2_data))
