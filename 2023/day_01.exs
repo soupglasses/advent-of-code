44833cd119f86bb3156a6d3da466b290fdc80672
Sofi <sofi+git@mailbox.org>
2023-12-02 02:20:41 +0100

    style: change indent slightly

--- 2023/day_01.exs
+++ 2023/day_01.exs
@@ -2,18 +2,18 @@
 
 defmodule Day01 do
   def parse(""), do: []
-  def parse(<<c>> <> rest) when c in ?0..?9, do: [c - ?0 | parse(rest)]
-  def parse("zero" <> rest),  do: [0 | parse("o" <> rest)]
-  def parse("one" <> rest),   do: [1 | parse("e" <> rest)]
-  def parse("two" <> rest),   do: [2 | parse("o" <> rest)]
+  def parse(<<c>>   <> rest) when c in ?0..?9, do: [c - ?0 | parse(rest)]
+  def parse("zero"  <> rest), do: [0 | parse("o" <> rest)]
+  def parse("one"   <> rest), do: [1 | parse("e" <> rest)]
+  def parse("two"   <> rest), do: [2 | parse("o" <> rest)]
   def parse("three" <> rest), do: [3 | parse("e" <> rest)]
-  def parse("four" <> rest),  do: [4 | parse("r" <> rest)]
-  def parse("five" <> rest),  do: [5 | parse("e" <> rest)]
-  def parse("six" <> rest),   do: [6 | parse("x" <> rest)]
+  def parse("four"  <> rest), do: [4 | parse("r" <> rest)]
+  def parse("five"  <> rest), do: [5 | parse("e" <> rest)]
+  def parse("six"   <> rest), do: [6 | parse("x" <> rest)]
   def parse("seven" <> rest), do: [7 | parse("n" <> rest)]
   def parse("eight" <> rest), do: [8 | parse("t" <> rest)]
-  def parse("nine" <> rest),  do: [9 | parse("e" <> rest)]
-  def parse(<<_>> <> rest),   do: parse(rest)
+  def parse("nine"  <> rest), do: [9 | parse("e" <> rest)]
+  def parse(<<_>>   <> rest), do: parse(rest)
 
   def solve(str) do
     String.split(str)
