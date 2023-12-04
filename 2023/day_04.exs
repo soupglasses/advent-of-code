ec8b31988ec17c47c8c8192f81f602f759a850a1
Sofi <sofi+git@mailbox.org>
2023-12-04 21:50:50 +0100

    style: small style changes

--- 2023/day_04.exs
+++ 2023/day_04.exs
@@ -5,40 +5,35 @@ Mix.install([
 ])
 
 defmodule Day04 do
-  def parse(records) do
+  def parse_card_wins(records) do
     {:ok, parsed, "", _, _, _} = Day04.Parser.from_string(records)
 
     Enum.map(parsed, fn {:game, game} ->
       {
         game[:id],
-        MapSet.intersection(
-          MapSet.new(game[:winning_numbers]),
-          MapSet.new(game[:hand])
-        )
-        |> Enum.count()
+        MapSet.new(game[:winning_numbers])
+        |> MapSet.intersection(MapSet.new(game[:numbers_we_have]))
+        |> MapSet.size()
       }
     end)
   end
 
   def part1(records) do
-    card_count_wins = parse(records)
-
-    card_count_wins
-    |> Enum.map(&(:math.pow(2, elem(&1, 1) - 1) |> Kernel.floor()))
+    parse_card_wins(records)
+    |> Enum.map(&floor(2 ** (elem(&1, 1) - 1)))
     |> Enum.sum()
   end
 
   def part2(records) do
-    card_count_wins = parse(records)
-    original_cards = Map.new(card_count_wins, &{elem(&1, 0), 1})
+    card_wins = parse_card_wins(records)
+    original_cards = Map.new(card_wins, &{elem(&1, 0), 1})
 
-    card_count_wins
-    |> Enum.reduce(original_cards, fn {id, count}, acc ->
+    Enum.reduce(card_wins, original_cards, fn {id, count}, acc ->
       Range.new(id + 1, id + count, 1)
       |> Enum.reduce(%{}, fn i, iacc ->
-        Map.merge(iacc, %{i => Map.get(acc, id, 1)})
+        Map.put(iacc, i, Map.get(acc, id, 1))
       end)
-      |> Map.merge(acc, fn _k, v1, v2 -> v1 + v2 end)
+      |> Map.merge(acc, fn _, v1, v2 -> v1 + v2 end)
     end)
     |> Map.values()
     |> Enum.sum()
@@ -48,13 +43,13 @@ end
 defmodule Day04.Parser do
   import NimbleParsec
 
-  round = fn type ->
-    times(
-      ignore(optional(string(" ")))
-      |> integer(min: 1, max: 2)
-      |> ignore(optional(string(" "))),
-      min: 1
-    )
+  number =
+    ignore(optional(string(" ")))
+    |> integer(min: 1, max: 2)
+    |> ignore(optional(string(" ")))
+
+  numbers = fn type ->
+    times(number, min: 1)
     |> tag(type)
   end
 
@@ -64,9 +59,9 @@ defmodule Day04.Parser do
     |> integer(min: 1)
     |> unwrap_and_tag(:id)
     |> ignore(string(": "))
-    |> concat(round.(:winning_numbers))
+    |> concat(numbers.(:winning_numbers))
     |> ignore(string("| "))
-    |> concat(round.(:hand))
+    |> concat(numbers.(:numbers_we_have))
     |> tag(:game)
     |> ignore(optional(string("\n")))
 
