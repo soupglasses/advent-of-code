91bd03f5918429a1ae503228441f63739dc31d78
Sofi <sofi+git@mailbox.org>
2024-07-03 01:40:39 +0200

    fix: day 6 to use bigint for arbitarily large numbers

--- 2023/crystal/day_06.cr
+++ 2023/crystal/day_06.cr
@@ -1,23 +1,25 @@
 #!/usr/bin/env crystal
 
-def next_digit(float : Float64) : Int64
-  float % 1 == 0.0 ? float.to_i64 + 1 : float.ceil.to_i64
+require "big"
+
+def next_digit(float : BigFloat) : BigInt
+  float.ceil.to_big_i
 end
 
-def prev_digit(float : Float64) : Int64
+def prev_digit(float : BigFloat) : BigInt
   -next_digit(-float)
 end
 
-def parse(data : String) : Array(Tuple(Int64, Int64))
+def parse(data : String) : Array(Tuple(BigInt, BigInt))
   time, distance = data.split("\n")
 
-  times = time[5..].split.map(&.to_i64)
-  distances = distance[9..].split.map(&.to_i64)
+  times = time[5..].split.map(&.to_big_i)
+  distances = distance[9..].split.map(&.to_big_i)
 
   times.zip(distances)
 end
 
-def total_faster_solutions(time_ms : Int64, record_distance : Int64) : Int64
+def total_faster_solutions(time_ms : BigInt, record_distance : BigInt) : BigInt
   # Find roots of: x^2 - tx + d < 0
   a, b, c = 1, -time_ms, record_distance
 
@@ -29,12 +31,13 @@ def total_faster_solutions(time_ms : Int64, record_distance : Int64) : Int64
   prev_digit(x_max) - next_digit(x_min) + 1
 end
 
-def part1(races : Array(Tuple(Int64, Int64))) : Int64
+def part1(races : Array(Tuple(BigInt, BigInt))) : BigInt
+  # TODO: Figure out why example gives the wrong answer for part1, but input works correctly.
   races.map { |race| total_faster_solutions(race[0], race[1]) }.product
 end
 
-def part2(races : Array(Tuple(Int64, Int64))) : Int64
-  race = races.transpose.map { |race| race.map(&.to_s).join("").to_i64 }
+def part2(races : Array(Tuple(BigInt, BigInt))) : BigInt
+  race = races.transpose.map { |race| race.map(&.to_s).join("").to_big_i }
   total_faster_solutions(race[0], race[1])
 end
 
