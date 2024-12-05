51b33c6f8185df2c991ebebfddeb10e588f31e53
Sofie <sofie+git@mailbox.org>
2024-12-05 11:39:14 +0000

    feat: simplify logic to avoid duplication of checks

--- 2024/ruby/day_05.rb
+++ 2024/ruby/day_05.rb
@@ -8,25 +8,23 @@ class Day05 < AoCDay
   def setup(data)
     rules, updates = data.split("\n\n")
 
-    @rules = rules.lines.map { |line| line.split("|").map(&:to_i) }
-    @rules_before = @rules.map(&:reverse)
-                          .group_by(&:first)
-                          .transform_values { |values| values.flat_map(&:last) }
-    @rules_after = @rules.group_by(&:first)
-                         .transform_values { |values| values.flat_map(&:last) }
-    @updates = updates.lines.map { |line| line.split(",").map(&:to_i) }
+    @rules = rules.lines.map { |line| line.split('|').map(&:to_i) }
+    @comes_before = @rules
+                    .map(&:reverse)
+                    .group_by(&:first)
+                    .transform_values { |values| values.flat_map(&:last) }
+
+    @updates = updates.lines.map { |line| line.split(',').map(&:to_i) }
   end
 
   def valid?(line)
-    line.partitions.all? do |left, current, right|
-      valid_left = left.all? { @rules_after[_1]&.include?(current) }
-      valid_right = right.all? { @rules_before[_1]&.include?(current) }
-      valid_left && valid_right
+    line.partitions.all? do |_, current, right|
+      right.all? { @comes_before[_1]&.include?(current) }
     end
   end
 
   def sort(line)
-    line.sort { |l, r| @rules_before[l]&.include?(r) ? 1 : -1 }
+    line.sort { |l, r| @comes_before[l]&.include?(r) ? 1 : -1 }
   end
 
   def part1
