37a62f36fe46a25572e4ae5c9f703ef4d1f058c6
Sofie <sofie+git@mailbox.org>
2024-12-05 12:21:36 +0000

    refactor: use comes_after instead of comes_before

--- 2024/ruby/day_05.rb
+++ 2024/ruby/day_05.rb
@@ -8,23 +8,24 @@ class Day05 < AoCDay
   def setup(data)
     rules, updates = data.split("\n\n")
 
-    @rules = rules.lines.map { |line| line.split('|').map(&:to_i) }
-    @comes_before = @rules
-                    .map(&:reverse)
-                    .group_by(&:first)
-                    .transform_values { |values| values.flat_map(&:last) }
+    @comes_after =
+      rules
+      .lines
+      .map { |line| line.split("|").map(&:to_i) }
+      .group_by(&:first)
+      .transform_values { |values| values.flat_map(&:last) }
 
-    @updates = updates.lines.map { |line| line.split(',').map(&:to_i) }
+    @updates = updates.lines.map { |line| line.split(",").map(&:to_i) }
   end
 
   def valid?(line)
-    line.partitions.all? do |_, current, right|
-      right.all? { @comes_before[_1]&.include?(current) }
+    line.partitions.all? do |left, current, _|
+      left.all? { @comes_after[_1]&.include?(current) }
     end
   end
 
   def sort(line)
-    line.sort { |l, r| @comes_before[l]&.include?(r) ? 1 : -1 }
+    line.sort { |l, r| @comes_after[l]&.include?(r) ? 1 : -1 }
   end
 
   def part1
